0847157919b91958b3b9ee27010755ec4d333321
Nerea Magaña <nereamga99@gmail.com>
2024-11-09 16:03:48 +0100

    He creado el atributo cadenaAMostrar para Color

--- src/main/java/org/iesalandalus/programacion/damas/Modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/damas/Modelo/Color.java
@@ -4,4 +4,7 @@ public enum Color {
     //Ponemos los colores que tendrán las Damas.
     BLANCO ("Blanco"),
     NEGRO ("Negro");
+
+    // Creamos el atributo cadenaAMostrar.
+    private final String cadenaAMostrar;
 }
