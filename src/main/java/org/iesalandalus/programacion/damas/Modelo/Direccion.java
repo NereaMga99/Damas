2150bc1af0c6b931215c1ba5bc38310253b57b94
Nerea Magaña <nereamga99@gmail.com>
2024-11-09 15:55:27 +0100

    Creo el enumerado Direccion con los literales

---
+++ src/main/java/org/iesalandalus/programacion/damas/Modelo/Direccion.java
@@ -0,0 +1,13 @@
+package org.iesalandalus.programacion.damas.Modelo;
+
+public enum Direccion {
+
+    // Aqui ponemos las direcciones que habrá en el tablero.
+    NORESTE("Noreste"),
+    SURESTE("Sureste"),
+    SUROESTE("Suroeste"),
+    NOROESTE("Noroeste");
+
+
+}
+
