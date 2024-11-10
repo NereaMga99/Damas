223aee207fa22e3a7b88cdff8897e4ec3ee4fa27
Nerea Magaña <nereamga99@gmail.com>
2024-11-10 19:13:48 +0100

    Se crea el Getter y el Setter de los atributos de la clase Dama.

--- src/main/java/org/iesalandalus/programacion/damas/Modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/Modelo/Dama.java
@@ -5,4 +5,40 @@ public class Dama {
     private Color color;
     private Posicion posicion;
     private boolean esDamaEspecial;
+
+    // Se crea el método Getter para que devuelva el valor de Color.
+    public Color getColor() {
+        return color;
+    }
+
+    // Aquí creamos el método Setter de Color.
+    public void setColor(Color color) {
+        if (color == null) {
+            throw new IllegalArgumentException("Error, el color de la Dama no puede ser nulo");
+        }
+        this.color = color;
+    }
+
+    // Se crea el método Getter para que devuelva el valor de Posición.
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    // Se crea el método Setter para la Posición de la Dama.
+    public void setPosicion(Posicion posicion) {
+        if (posicion == null) {
+            throw new IllegalArgumentException("Error, la posición de la Dama no puede ser nula");
+        }
+        this.posicion = posicion;
+    }
+
+    // Aquí creamos el Getter de DamaEspecial.
+    public boolean getEsDamaEspecial() {
+        return esDamaEspecial;
+    }
+
+    // Creamos el Setter de DamaEspecial.
+    public void setEsDamaEspecial(boolean esDamaEspecial) {
+        this.esDamaEspecial = esDamaEspecial;
+    }
 }
