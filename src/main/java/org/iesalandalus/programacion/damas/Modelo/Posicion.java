3f6d24e43c93eacc96811cb3e31e6cf3c2940ac1
Nerea Magaña <nereamga99@gmail.com>
2024-11-10 11:41:45 +0100

    Creacion de clase Posicion con sus atributos.

---
+++ src/main/java/org/iesalandalus/programacion/damas/Modelo/Posicion.java
@@ -0,0 +1,8 @@
+package org.iesalandalus.programacion.damas.Modelo;
+
+public class Posicion {
+
+    // Creacion de los atributos, los ponemos privados para controlar el acceso y que esté más restringido.
+    private int fila;
+    private char columna;
+}
