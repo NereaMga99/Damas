4803b1c8b0694b9b6fd5cf6e6c8c9fd1a49dd2df
Nerea Magaña <nereamga99@gmail.com>
2024-11-13 18:44:58 +0100

    Creación del método mostrarMenu.

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -5,6 +5,14 @@ public class Consola {
 
     }
 
-
+    // Creamos el método mostrarMenú con las opciones que tendrá la aplicación.
+    public static void mostrarMenu() {
+        System.out.println("----------Menú de opciones----------");
+        System.out.println("1. Crear Dama por defecto");
+        System.out.println("2. Crear Dama eligiendo el color");
+        System.out.println("3. Mover");
+        System.out.println("4. Salir");
+        System.out.println("Elige una opción:");
+    }
 
 }
