8b6d3d610ed13b90cc3486ff92818952d05676b7
Nerea Magaña <nereamga99@gmail.com>
2024-11-13 19:51:37 +0100

    Se crea el método elegirOpcion para elegir el color de la Dama.

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -26,4 +26,19 @@ public class Consola {
         } while (opcion < 1 || opcion > 4);
         return opcion;
     }
+
+    // Creamos el método elegirOpcion para que elijamos el color de la Dama.
+    public static Color elegirOpcion() {
+        int colorDama;
+        System.out.println("Selecciona 1 para tener la Dama blanca y 2 para tener la Dama negra");
+        do { // Esto es lo que se va a repetir mientras se cumpla el while.
+            System.out.println("Elije un color para la Dama: ");
+            colorDama = Entrada.entero();
+        } while (colorDama < 1 || colorDama > 2);
+        if (colorDama == 1) { // Aqui hacemos el if ya que si elige el numero 1 automaticamente le devuelve la dama blanca.
+            return Color.BLANCO;
+        } else { // El else para que si no elige el número 1 lo detecte como el 2 para la dama negra.
+            return Color.NEGRO;
+        }
+    }
 }
