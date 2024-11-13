c7901eaa70124e3ce264a1197c51e78fd23e3a65
Nerea Magaña <nereamga99@gmail.com>
2024-11-13 19:32:35 +0100

    Se crea el método elegirOpcionMenu.

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -1,5 +1,8 @@
 package org.iesalandalus.programacion.damas;
 
+import org.iesalandalus.programacion.damas.Modelo.Color;
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
     private Consola () {
 
@@ -12,7 +15,15 @@ public class Consola {
         System.out.println("2. Crear Dama eligiendo el color");
         System.out.println("3. Mover");
         System.out.println("4. Salir");
-        System.out.println("Elige una opción:");
     }
 
+    // Aquí creamos el método elegirOpcionMenu.
+    public static int elegirOpcionMenu() {
+        int opcion;
+        do {
+            System.out.println("Elije una opción: ");
+            opcion= Entrada.entero();
+        } while (opcion < 1 || opcion > 4);
+        return opcion;
+    }
 }
